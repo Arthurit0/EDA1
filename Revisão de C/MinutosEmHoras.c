97ff4409fb8871d7182d3d88ba027a9aa3ea1b64
Arthur0 <57883830+Arthurit0@users.noreply.github.com>
2021-10-11 12:54:40 -0300

    Create MinutosEmHoras.c

---
+++ Revisão de C/MinutosEmHoras.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+struct hm{
+    int horas;
+    int minutos;
+};
+
+int main(){
+    int m;
+    struct hm minParaHoras;
+
+    printf("Digite o tempo em minutos\n");
+    scanf("%d",%minutos);
+
+    minParaHoras.horas = m/60;
+    minParaHoras.minutos = m%60;
+
+    printf("Sao %d horas e %d minutos", minParaHoras.horas, minParaHoras.minutos);
+
+    return 0;
+}
