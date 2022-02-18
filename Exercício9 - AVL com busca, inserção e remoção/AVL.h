dbf2cb94951291b0d835aaf92091e42076c7043d
Your Name <you@example.com>
2022-02-17 22:12:46 -0300

    finalizando arquivos AVL

--- Exercício9 - AVL com busca, inserção e remoção/AVL.h
+++ Exercício9 - AVL com busca, inserção e remoção/AVL.h
@@ -1,6 +1,6 @@
-typedef struct nodo nodo;
+typedef struct no nodo;
 
-struct nodo{
+struct no{
     int chave;
     nodo *esq;
     nodo *dir;
@@ -11,5 +11,14 @@ typedef nodo * arvore;
 
 //novo é um ponteiro para um nó já alocado
 nodo * cria_no(int chave);
-arvore insere(arvore a, nodo *novo);
-nodo * busca(arvore a, int chave);
+arvore insere(arvore a, nodo * novo);
+arvore remove_nodo(nodo * q);
+int altura(arvore a);
+int fator_balanceamento(arvore a);
+arvore balanceamento(arvore a);
+arvore rotDir(nodo *p);
+arvore rotEsq(nodo * p);
+arvore busca_r(arvore a, int k);
+arvore busca_i(arvore a, int k); //Sem balanceamento
+arvore busca_remove_r(arvore a, int k);
+arvore busca_remove_i(arvore a, int k);
